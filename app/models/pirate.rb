66acfe6a767c88fafdb78bc35ade85902b5c08ea
Mary Jackson <jackson.marytherese@gmail.com>
2018-07-17 23:50:47 +0000

    automatically backed up by learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -13,5 +13,6 @@ class Pirate
 
   def self.all
     @@all
+  end
 
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,4 +1,18 @@
 class Ship
 
-  
+  attr_accessor :name, :weight, :height
+
+  @@all = []
+
+  def initialize(params)
+    @name = params[:name]
+    @weight = params[:weight]
+    @height = params[:height]
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
 end
