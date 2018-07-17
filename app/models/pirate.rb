d0c2ab75e74a761fe3534dd9c6bd5ea443764cb7
Mary Jackson <jackson.marytherese@gmail.com>
2018-07-17 23:48:49 +0000

    automatically backed up by learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -2,8 +2,13 @@ class Pirate
 
   attr_accessor :name, :weight, :height
 
+  @@all = []
+
   def initialize(params)
     @name = params[:name]
+    @weight = params[:weight]
+    @height = params[:height]
+
   end
 
 end
