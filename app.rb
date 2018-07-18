9cfcf0198a280c6f9eecc33ee3d599e429650d1b
Mary Jackson <jackson.marytherese@gmail.com>
2018-07-18 00:23:58 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -14,7 +14,7 @@ module FormsLab
     post '/pirates' do
       @pirate = Pirate.new(params[:pirate])
 
-      params[:pirate][:ship].each do |details|
+      params[:pirate][:ships].each do |details|
         Ship.new(details)
       end
 
