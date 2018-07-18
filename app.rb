6cdefada078af77c5ad27ebd51b330de47da0e03
Mary Jackson <jackson.marytherese@gmail.com>
2018-07-18 00:20:42 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -12,8 +12,8 @@ module FormsLab
     end
 
     post '/pirates' do
-      @pirate = Pirate.new(params[pirate])
-      
+      @pirate = Pirate.new(params[:pirate])
+
 
       erb :"pirates/show"
     end
